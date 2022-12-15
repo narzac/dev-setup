20921ac0fce0243a69fb24653eba33be0e815eeb
narzac <ahmethegreen@gmail.com>
2022-12-15 15:39:21 +0100

    wip2

--- osx-play.sh
+++ osx-play.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# TODO: Here install the homebrew by first checking if it is already installed
-# bash ./install-homebrew.sh
+# Make bash default shell
+chsh -s /bin/bash
 
-# brew install python
-# brew install ansible
+# TODO: Here install the homebrew by first checking if it is already installed
+bash ./install-homebrew.sh
+
+# adjust paths for homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install python
+brew install ansible
 
 ansible-playbook --ask-vault-pass osx-playbook.yml ## --ask-become-pass
